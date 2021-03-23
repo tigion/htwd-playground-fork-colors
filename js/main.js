fa377854e910d3148bf39caf02497fc668eaee90
Christoph Zirkelbach <christoph.zirkelbach@htw-dresden.de>
2021-03-23 15:41:54 +0100

    Initial import

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// load json function
+
+function loadColors() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var recordCount = 0;
+            var records = JSON.parse(this.responseText);
+            var newInnerHTML = '';
+            records.forEach(function (record, index) {
+                recordCount++;
+
+                // get values
+                var id = String(record.id).trim();
+                var r = colorValue(record.color.r);
+                var g = colorValue(record.color.g);
+                var b = colorValue(record.color.b);
+                var rgbColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+
+                // set output
+                if (id != "") {
+                    newInnerHTML += '<span title="' + escapeHtml(id) + '" style="background-color: ' + escapeHtml(rgbColor) + '">';
+                    newInnerHTML += '</span>';
+                }
+            });
+            if (recordCount > 0)
+                document.getElementById("colorRecords").innerHTML = newInnerHTML;
+        }
+    };
+    xmlhttp.open("GET", "data/colors.json", true);
+    xmlhttp.send();
+}
+
+// helper functions
+
+function escapeHtml(text) {
+    var map = { '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#039;' };
+    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+
+function colorValue(value) {
+    var x = parseInt(value);
+    return (x < 0) ? 0 : (x > 255) ? 255 : x;
+}
