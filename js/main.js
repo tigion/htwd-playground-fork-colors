db9d069eb64b10992ad8f6a1beb9f1fa9cc735fd
Christoph Zirkelbach <christoph.zirkelbach@htw-dresden.de>
2021-05-04 09:36:02 +0200

    Add color picker

--- js/main.js
+++ js/main.js
@@ -7,22 +7,22 @@ function loadColors() {
             var recordCount = 0;
             var records = JSON.parse(this.responseText);
             var newInnerHTML = '';
-            records.forEach(function (record, index) {
 
-                // get values
+            // process records
+            records.forEach(function (record, index) {
+                // get id and skip blank ids
                 var id = String(record.id).trim();
+                if (id == "") return;
+                // get RGB color values
                 var r = colorValue(record.color.r);
                 var g = colorValue(record.color.g);
                 var b = colorValue(record.color.b);
-                var rgbColor = 'rgb(' + r + ',' + g + ',' + b + ')';
-
-                // set output
-                if (id != "") {
-                    recordCount++;
-                    newInnerHTML += '<span title="' + escapeHtml(id) + '\n' + escapeHtml(rgbColor) + '" style="background-color: ' + escapeHtml(rgbColor) + '">';
-                    newInnerHTML += '</span>';
-                }
+                // set HTML output
+                newInnerHTML += recordAsHTML(id, r, g, b)
+                // count records found
+                recordCount++;
             });
+            // message for no records
             if (recordCount == 0)
                 newInnerHTML = 'No colors found! 😭';
 
@@ -34,9 +34,58 @@ function loadColors() {
     xmlhttp.send();
 }
 
+// choose color functions
+
+function handleColorChange(e) {
+    var value = colorValue(e.value);
+    if (e.value != value.toString())
+        e.value = value;
+    updateColorPreview();
+}
+
+function handleRandomColor(e) {
+    // limit random values between min and max
+    const min = 50;  // 0
+    const max = 255; // 255
+    document.getElementById("r").value = colorValue(Math.floor(Math.random() * (max - min)) + min);
+    document.getElementById("g").value = colorValue(Math.floor(Math.random() * (max - min)) + min);
+    document.getElementById("b").value = colorValue(Math.floor(Math.random() * (max - min)) + min);
+    updateColorPreview();
+}
+
+function updateColorPreview() {
+    var r = colorValue(document.getElementById("r").value);
+    var g = colorValue(document.getElementById("g").value);
+    var b = colorValue(document.getElementById("b").value);
+    var color = RGBColorValue(r, g, b);
+    var title = titleAttribute("Preview", color);
+    var element = document.getElementById("colorPreview");
+    element.style["background-color"] = color;
+    element.title = title;
+    document.getElementById("colorPreviewJson").innerHTML = recordAsJSON(r, g, b);
+}
+
 // helper functions
 
-function escapeHtml(text) {
+function RGBColorValue(r, g, b) {
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
+
+function titleAttribute(id, color) {
+    return escapeHTML(id) + '\n' + escapeHTML(color);
+}
+
+function recordAsHTML(id, r, g, b) {
+    var color = RGBColorValue(r, g, b);
+    var title = titleAttribute(id, color);
+    return '<span title="' + title + '" style="background-color: ' + color + '"></span>';
+}
+
+function recordAsJSON(r, g, b) {
+    return '"color": { "r": ' + r + ', "g": ' + g + ', "b": ' + b + ' }';
+}
+
+function escapeHTML(text) {
     var map = { '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#039;' };
     return text.replace(/[&<>"']/g, function(m) { return map[m]; });
 }
